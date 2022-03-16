ca7eb646981595d0f5ee111ad92ad7bce271016e
JongZero <whddud7082@gmail.com>
2022-03-17 00:10:39 +0900

    03/17
    12시 넘어서 1010번 문제 풀이 완료

--- OneDay_OneAlgorithm/1010.cpp
+++ OneDay_OneAlgorithm/1010.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
 
-unsigned long long factorial(const int n)
-{
-	unsigned long result = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		result *= i;
-	}
-	return result;
-}
+// 1. 처음에 mCn 콤비네이션 문제인지 모르고 Func함수를 이용한 재귀함수로 풀려고 시도
+// 2. 그 다음으로는 mCn 공식대로 팩토리얼 함수 만들어서 푸는데 값이 너무컸다.
+// 3. 세번째로는 팩토리얼을 바로 구하는게 아니라 3! 이면 벡터에 3,2,1 넣어놓고 계속해서 분모/분자
+//	  하는 방식으로 풀었더니 성공!
 
 void Func(int n, int m, std::vector<int> vec, int& count)
 {
@@ -29,20 +23,6 @@ void Func(int n, int m, std::vector<int> vec, int& count)
 		check = true;
 	}
 
-	// 배열의 다음이 이전보다 큰 지 확인
-	//if (check == false)
-	//{
-	//	int prevNum = vec.front();
-	//	for (int i = 1; i < vec.size(); i++)
-	//	{
-	//		if (vec[i] <= prevNum)
-	//		{
-	//			check = true;
-	//			break;
-	//		}
-	//	}
-	//}
-	
 	// 모든 조건 통과
 	if (check == false)
 	{
@@ -77,9 +57,14 @@ int main()
 		int n, m;
 		std::cin >> n >> m;
 
+		if (n == m)
+		{
+			std::cout << 1 << '\n';
+			continue;
+		}
+		
 		// 분자
 		std::vector<int> numeratorVec;
-		//unsigned long long numerator = 1;
 		for (int i = n + 1; i <= m; i++)
 		{
 			numeratorVec.emplace_back(i);
@@ -87,7 +72,7 @@ int main()
 
 		// 분모
 		std::vector<int> denominatorVec;
-		for (int i = 2; i <= m - n; i++)
+		for (int i = 1; i <= m - n; i++)
 		{
 			denominatorVec.emplace_back(i);
 		}
@@ -100,35 +85,40 @@ int main()
 			// 나눠 떨어진다면
 			if (numeratorVec[nIndex] % denominatorVec[dIndex] == 0)
 			{
-				
+				numeratorVec[nIndex] = numeratorVec[nIndex] / denominatorVec[dIndex];
+
+				denominatorVec[dIndex] = 1;
+			}
+
+			dIndex++;
+
+			if (dIndex >= denominatorVec.size())
+			{
+				dIndex = 0;
+				nIndex++;
+
+				if (nIndex >= numeratorVec.size())
+				{
+					check = false;
+				}
 			}
 		}
 
-		int a = 0;
+		unsigned long long numerator = 1;
+		for (int i = 0; i < numeratorVec.size(); i++)
+		{
+			numerator *= numeratorVec[i];
+		}
 
-		//unsigned long long denominator = factorial(m - n);
+		unsigned long long denominator = 1;
+		for (int i = 0; i < denominatorVec.size(); i++)
+		{
+			denominator *= denominatorVec[i];
+		}
 
-		//unsigned long long result = numerator / denominator;
+		unsigned long long result = numerator / denominator;
 
-		//unsigned long long numerator = factorial(m);
-
-		// 분모
-		//unsigned long long denominator = factorial(m - n) * factorial(n);
-
-		//unsigned long long result = numerator / denominator;
-		//std::cout << result << '\n';
-
-		//std::vector<int> vec(n);
-		//
-		//for (int j = 0; j < n; j++)
-		//{
-		//	vec[j] = j;
-		//}
-		//
-		//int count = 0;
-		//Func(n, m, vec, count);
-		//
-		//std::cout << count << '\n';
+		std::cout << result << '\n';
 	}
 
 	return 0;
