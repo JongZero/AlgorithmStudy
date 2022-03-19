54a83a7074981c8afd5d90210b8f41ee1661fc6d
JongZero <whddud7082@gmail.com>
2022-03-20 00:08:33 +0900

    03/19
    스택 문제 풀이 완료

--- OneDay_OneAlgorithm/9012.cpp
+++ OneDay_OneAlgorithm/9012.cpp
@@ -11,37 +11,47 @@ int main()
 
 	for (int i = 0; i < t; i++)
 	{
+		// 스택 초기화
 		int stack[51] = { 0, };
 
+		// Input 받기
 		std::string input;
 		std::cin >> input;
 
+		// 스택에 현재 넣어야할 인덱스
 		int nowIndex = 0;
+
+		// 스택의 현재 사이즈
 		int stackSize = 0;
-		bool isClose = false;
+
 		for (int k = 0; k < input.size(); k++)
 		{
-			if (input[i] == '(')
+			// '(' 일 때 push하고, 아닐 때 pop하면 정상이라면 스택의 사이즈가 0일 것임
+			if (input[k] == '(')
 			{
 				stack[nowIndex] = 1;
 				nowIndex++;
 				stackSize++;
-				isClose = false;
+			}
+			else
+			{
+				if (stackSize > 0)
+				{
+					stack[nowIndex - 1] = 0;
+					nowIndex--;
+				}
 
-				if (isClose == true)
+				stackSize--;
+
+				// 스택의 사이즈가 0보다 작을 경우 그 시점에서 이미 이상하다.
+				if (stackSize < 0)
 				{
 					break;
 				}
 			}
-			else if (stackSize > 0)
-			{
-				isClose = true;
-				stack[nowIndex - 1] = 0;
-				nowIndex--;
-				stackSize--;
-			}
 		}
 
+		// 0이면 정상, 0이 아니면 비정상
 		if (stackSize == 0)
 		{
 			std::cout << "YES\n";
@@ -51,8 +61,6 @@ int main()
 			std::cout << "NO\n";
 		}
 	}
-
-
 
 	return 0;
 }
