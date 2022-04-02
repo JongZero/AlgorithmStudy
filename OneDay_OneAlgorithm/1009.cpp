6b4721e381c81b648f0fd59d016894607cba8685
JongZero <whddud7082@gmail.com>
2022-04-03 01:16:06 +0900

    04/03
    백준 1009 : 분산처리 풀이 완료
    브론즈문제 중에 제일 어려운 것 같다...

--- OneDay_OneAlgorithm/1009.cpp
+++ OneDay_OneAlgorithm/1009.cpp
@@ -104,6 +104,14 @@ int main()
 	}
 	*/
 
+	// 정답 버전
+	// 아이디어 : 최소한의 계산 횟수를 먼저 구하고, 계산한다. (지수법칙활용)
+	// ex) 2의 100제곱 (계산 횟수 : 100번)
+	// 2의 100제곱 = 2의 제곱의 50제곱 (51번 = 제곱 1번 + 50제곱 50번)
+	// 2의 제곱의 50제곱 = 2의 제곱의 제곱의 25제곱 (27번 = 제곱 2번 + 25제곱 25번)
+	// 2의 제곱의 제곱의 25제곱 = 2의 제곱의 제곱의 제곱의 12제곱 * 2의 4제곱 (19번 = 제곱 3번 + 12제곱 12번 + 나머지 4번)
+	// 이런식으로 계산 횟수가 최소가 되는 것을 구해서 그에 맞는 계산을 해준다.
+	// 말로 설명하기가 어렵다...
 	int t = 0;
 	std::cin >> t;
 
@@ -112,36 +120,114 @@ int main()
 		int a, b;
 		std::cin >> a >> b;
 
+		// 몇 번 제곱을 해줄지 카운트
+		int bCount = 0;
+
+		// bCount만큼 제곱을 하고나서 남는 숫자,
+		// a의 remainderCount제곱을 구할 것이다.
+		int remainder = 1;
+		int remainderCount = 0;
+
+		// 계산 횟수
+		int prev = 1000000;
+		int now = 0;
+
+		while (true)
+		{
+			if (b < 2)
+			{
+				break;
+			}
+
+			// 홀수라면 나머지가 생긴다. (나머지는 2의 이전의 제곱 횟수 제곱)
+			if (b % 2 == 1)
+			{
+				remainderCount += remainder;
+			}
+
+			int prevB = b;
+			b /= 2;
+			bCount++;
+			remainder = pow(2, bCount);
+
+			now = bCount + b + remainderCount;
+
+			// 현재 횟수가 이전의 횟수보다 많다면
+			// 이전의 횟수가 최소 횟수임
+			if (now > prev)
+			{
+				b = prevB;
+				bCount--;
+				remainderCount -= pow(2, bCount);
+				break;
+			}
+
+			prev = now;
+		}
+
+		/// 제곱부터 해준다.
 		int mul = a;
 		char mulChar = '1';
 		std::string mulStr = "";
-		
-		while (b >= 1)
-		{	
-			// 제곱
-			// char to int
+		for (int j = 0; j < bCount; j++)
+		{
 			mulStr = std::to_string(mul);
 			mulChar = mulStr[mulStr.length() - 1];
+
+			// char to int
 			mul = mulChar - '0';
 			mul *= mul;
-
-			// 제곱했으므로 지수는 계속 2로 나눠줌
-			b /= 2;
 		}
 
-		// 마지막 자리수만 남긴다.
+		/// b번만큼 mul을 제곱
+		// 맨 마지막 자리수만 필요
 		mulStr = std::to_string(mul);
 		mulChar = mulStr[mulStr.length() - 1];
-		int result = mulChar - '0';
+		mul = mulChar - '0';
+		int bResult = mul;
+		for (int j = 0; j < b - 1; j++)
+		{
+			mulStr = std::to_string(mul);
+			mulChar = mulStr[mulStr.length() - 1];
 
-		if (result == 0)
+			// char to int
+			mul = mulChar - '0';
+			mul *= bResult;
+		}
+
+		// 맨 마지막 자리수만 필요
+		mulStr = std::to_string(mul);
+		mulChar = mulStr[mulStr.length() - 1];
+		mul = mulChar - '0';
+
+		/// 나머지 계산 (a의 remainderCount 제곱)
+		int remainderResult = 1;
+		for (int j = 0; j < remainderCount; j++)
+		{
+			mulStr = std::to_string(remainderResult);
+			mulChar = mulStr[mulStr.length() - 1];
+
+			// char to int
+			remainderResult = mulChar - '0';
+			remainderResult *= a;
+		}
+		
+		// 둘이 합친다.
+		mul *= remainderResult;
+
+		// 맨 마지막 자리수를 마지막으로 구한다.
+		mulStr = std::to_string(mul);
+		mulChar = mulStr[mulStr.length() - 1];
+		mul = mulChar - '0';
+
+		// 0이면 10번 컴퓨터
+		if (mul == 0)
 		{
 			std::cout << 10 << '\n';
 		}
 		else
 		{
-			// 출력
-			std::cout << result << '\n';
+			std::cout << mul << '\n';
 		}
 	}
 
