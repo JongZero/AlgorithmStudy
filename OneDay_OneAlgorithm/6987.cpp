22e6f484cbe0f46f5c7d615c4a5a72ce3da41d43
JongZero <whddud7082@gmail.com>
2022-05-07 23:41:30 +0900

    05/07
    백준 6987번 푸는 중

---
+++ OneDay_OneAlgorithm/6987.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+
+
+	return 0;
+}
