a60f75c90d07fc49eecb214451f53148c294d298
JongZero <whddud7082@gmail.com>
2022-05-23 23:04:49 +0900

    05/23
    14889 문제 풀이 완료

--- OneDay_OneAlgorithm/14889_2.cpp
+++ OneDay_OneAlgorithm/14889_2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 typedef pair<int, int> pairIndex;
-vector<bool> isVisited;
 
 int main()
 {
@@ -16,9 +17,7 @@ int main()
 	int n = 0;
 	cin >> n;
 
-	isVisited.resize(n);
-
-	unordered_map<pairIndex, int> m;
+	map<pairIndex, int> m;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -31,7 +30,54 @@ int main()
 		}
 	}
 
+	// n / 2¸¸Å­ »Ì´Â´Ù.
+	// 4 -> 2, 6 -> 3
+	vector<int> nVec(n);
+	for (int i = n / 2; i < n; i++)
+	{
+		nVec[i] = 1;
+	}
 
+	int min = 100000000;
+	do 
+	{
+		vector<int> choiceVec;
+		vector<int> notChoiceVec;
+
+		// ÆÀÀ» »Ì´Â´Ù.
+		for (int i = 0; i < n; i++)
+		{
+			if (nVec[i] == 1)
+				choiceVec.push_back(i);
+			else
+				notChoiceVec.push_back(i);
+		}
+
+		int choiceAbility = 0;
+		for (int i = 0; i < n / 2; i++)
+		{
+			for (int j = 0; j < n / 2; j++)
+			{
+				choiceAbility += m[pairIndex(choiceVec[i], choiceVec[j])];
+			}
+		}
+
+		int notChoiceAbility = 0;
+		for (int i = 0; i < n / 2; i++)
+		{
+			for (int j = 0; j < n / 2; j++)
+			{
+				notChoiceAbility += m[pairIndex(notChoiceVec[i], notChoiceVec[j])];
+			}
+		}
+
+		int sub = abs(notChoiceAbility - choiceAbility);
+		if (min > sub)
+			min = sub;
+
+	} while (next_permutation(nVec.begin(), nVec.end()));
+
+	cout << min;
 
 	return 0;
 }
