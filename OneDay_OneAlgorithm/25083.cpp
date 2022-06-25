7a298802afef8b43ec3777bc1018de5cde4b22c0
JongZero <whddud7082@gmail.com>
2022-06-25 21:16:49 +0900

    06/25
    백준 25083 기분 전환용으로 품

---
+++ OneDay_OneAlgorithm/25083.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |";
+
+
+	return 0;
+}
