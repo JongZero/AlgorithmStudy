14a7dd0a9572b2e41a24b797faa6aaddc1bd819b
JongZero <whddud7082@gmail.com>
2022-09-13 01:00:12 +0900

    09/13
    7주차 복습

---
+++ OneDay_OneAlgorithm/7-B-7.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a[17][17], dp[17][17][3];
+enum eType
+{
+	G,
+	S,
+	D,
+};
+bool check(int y, int x, eType t)
+{
+	if (a[y][x]) return false;
+	if (t == D)
+	{
+		if (a[y - 1][x] || a[y][x - 1]) return false;
+	}
+	return true;
+}
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	dp[1][2][G] = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			// 가로
+			if (check(i, j + 1, G)) dp[i][j + 1][G] += dp[i][j][G];
+			if (check(i + 1, j + 1, D)) dp[i + 1][j + 1][D] += dp[i][j][G];
+
+			// 세로
+			if (check(i + 1, j, S)) dp[i + 1][j][S] += dp[i][j][S];
+			if (check(i + 1, j + 1, D)) dp[i + 1][j + 1][D] += dp[i][j][S];
+
+			// 대각
+			if (check(i, j + 1, G)) dp[i][j + 1][G] += dp[i][j][D];
+			if (check(i + 1, j, S)) dp[i + 1][j][S] += dp[i][j][D];
+			if (check(i + 1, j + 1, D)) dp[i + 1][j + 1][D] += dp[i][j][D];
+		}
+	}
+	cout << dp[n][n][G] + dp[n][n][S] + dp[n][n][D];
+}
---
+++ OneDay_OneAlgorithm/7-I-4.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[10001];
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(NULL);
+
+	while (true)
+	{
+		int n, m1, m2, m;
+		scanf("%d %d.%d", &n, &m1, &m2);
+		if (n == 0) break;
+		m = m1 * 100 + m2;
+		fill(dp, dp + 10001, 0);
+		for (int i = 0; i < n; i++)
+		{
+			int c, p1, p2, p;
+			scanf("%d %d.%d", &c, &p1, &p2);
+			p = p1 * 100 + p2;
+			for (int j = p; j <= m; j++)
+			{
+				dp[j] = max(dp[j], dp[j - p] + c);
+			}
+		}
+		cout << dp[m] << '\n';
+	}
+}
