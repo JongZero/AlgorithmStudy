2d68f007b0cd98616f5ca9789ea194876387a208
JongZero <whddud7082@gmail.com>
2022-09-27 23:48:20 +0900

    09/27
    프로그래머스2

---
+++ OneDay_OneAlgorithm/7-프로그래머스-2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, num;
+
+int solution(int N, int number) {
+	int answer = -1;
+
+
+
+	return answer;
+}
+
+void main()
+{
+	cout << "ANSWER : " << solution(3, 7);
+}
