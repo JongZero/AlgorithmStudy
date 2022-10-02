675fe6cc587d7dd236937472ba50e76079fb84cd
JongZero <whddud7082@gmail.com>
2022-10-02 23:28:59 +0900

    10/02
    dynamic_cast test2
    가상함수가 없는 경우 업캐스팅만 가능

--- C++Study/dynamic_cast_Test.cpp
+++ C++Study/dynamic_cast_Test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Base
 {
 public:
-	virtual void Func();
+	void baseFunc();
 };
 
 class De : public Base
@@ -15,8 +15,13 @@ public:
 
 int main()
 {
+	// 다이나믹 캐스트는 부모 클래스에 가상 함수가 없는 경우, 다운 캐스팅 불가능.
 	Base* b = new De;
 	dynamic_cast<De*>(b)->Func();
+
+	// 그러나, 업 캐스팅은 가능함.
+	De* d = new De;
+	dynamic_cast<Base*>(d)->baseFunc();
 
 	int a = 0;
 	string str = static_cast<string>(a);
