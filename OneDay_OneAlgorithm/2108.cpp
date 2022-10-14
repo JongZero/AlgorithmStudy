2c6ab5f791fb2fd58e4eaf83786b974bd7107f5c
JongZero <whddud7082@gmail.com>
2022-10-14 23:39:44 +0900

    10/14
    백준 2108 풀이

--- OneDay_OneAlgorithm/2108.cpp
+++ OneDay_OneAlgorithm/2108.cpp
@@ -2,99 +2,53 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int n;
+double sum;
+vector<int> a;
+unordered_map<int, int> mp;
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	const int Max = 8001;
-
-	int countArray[Max] = { 0, };
-
-	int n = 0;
-	std::cin >> n;
-	int* numsArray = new int[n];
-	
+	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		std::cin >> numsArray[i];
-		numsArray[i] += 4000;
-
-		countArray[numsArray[i]]++;
+		int t;
+		cin >> t;
+		a.push_back(t);
+		mp[t]++;
+		sum += t;
 	}
 
-	// 일단 카운팅 정렬 + 최다 카운트(최빈값) 찾기
-	int maxCount = 0;
-	std::vector<int> maxCountIndexSameVec;
-	for (int i = 0; i < Max; i++)
+	sort(a.begin(), a.end());
+
+	int nowMax = 0;
+	vector<int> v;
+	for (auto& it : mp)
 	{
-		if (maxCount < countArray[i])
+		if (it.second > nowMax)
 		{
-			maxCount = countArray[i];
-			maxCountIndexSameVec.clear();
-			maxCountIndexSameVec.push_back(i);
+			nowMax = it.second;
+			v.clear();
+			v.push_back(it.first);
 		}
-		// 여러 개 있을 경우
-		else if (maxCount == countArray[i])
+		else if (it.second == nowMax)
 		{
-			maxCountIndexSameVec.push_back(i);
+			v.push_back(it.first);
 		}
-
-		if (i != 0)
-			countArray[i] = countArray[i - 1] + countArray[i];
 	}
-	
-	// 카운팅 정렬 + 합 구하기 + 최대, 최소 구하기
-	int* sortArray = new int[n];
-	
-	double sum = 0;
-	int minNum = 4000;
-	int maxNum = -4000;
-	for (int i = n - 1; i >= 0; i--)
-	{
-		int count = countArray[numsArray[i]];
-		countArray[numsArray[i]]--;
-		sortArray[count - 1] = numsArray[i];
+	sort(v.begin(), v.end());
 
-		// 인덱스가 아닌 진짜 숫자
-		int num = numsArray[i] - 4000;
-		sum += num;
-
-		if (num < minNum)
-			minNum = num;
-
-		if (num > maxNum)
-			maxNum = num;
-	}
-	
-	/// 반올림한 산술평균
-	sum /= (double)n;
-	double average = round(sum);
-	std::cout << average << "\n";
-	
-	/// 중앙값
-	int centerIndex = n / 2;
-	int center = sortArray[centerIndex] - 4000;
-	std::cout << center << "\n";
-
-	/// 최빈값
-	int maxCountNum = 0;
-	// 2개 이상일 경우
-	if (maxCountIndexSameVec.size() > 1)
-	{
-		sort(maxCountIndexSameVec.begin(), maxCountIndexSameVec.end());
-
-		maxCountNum = maxCountIndexSameVec[1] - 4000;
-	}
-	else
-	{
-		maxCountNum = maxCountIndexSameVec[0] - 4000;
-	}
-	std::cout << maxCountNum << "\n";
-
-	/// 범위
-	std::cout << maxNum - minNum << "\n";
+	cout << floor(sum / (double)n + 0.5) << '\n';
+	cout << a[a.size() / 2] << '\n';
+	if (v.size() > 1) cout << v[1] << '\n';
+	else cout << v[0] << '\n';
+	cout << a.back() - a.front() << '\n';
 
 	return 0;
 }
